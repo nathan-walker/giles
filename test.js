8b0807beb834c6750a79d51d862b5efcaeb7b88d
Nathan Walker <me@nwalker.org>
2016-02-21 12:38:53 -0500

    Added a test

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const Request = require('./lib/request');
 const Agent = require('./lib/agent');
 
-var url = require('url').parse("http://nwalker.org/");
+var url = require('url').parse("http://nwalker.org");
 var agent = new Agent({redisConnection: {}});
 
 var req = new Request(url, undefined, agent);
 req.exec().then((data) => {
 	debugger;
+}).catch((err) => {
+	throw err;
 });
