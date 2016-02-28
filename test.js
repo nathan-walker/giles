af7fbbfc24ce9db67189504c513a8b45a235a6c4
Nathan Walker <me@nwalker.org>
2016-02-28 17:00:59 -0500

    Worked out some bugs for odd cases

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const Request = require('./lib/request');
 const Agent = require('./lib/agent');
 
-var url = require('url').parse("http://nwalker.org/contact");
+var url = require('url').parse("https://www.youtube.com/watch?v=e5R75lCSNEA");
 var agent = new Agent({redisConnection: {}});
 
 /*var req = new Request(url, undefined, agent);
